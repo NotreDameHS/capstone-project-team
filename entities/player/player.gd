3190b6586b9967c0a70122c0ece2f82d042e3c6b
ojrkarganilla <ojrkarganilla@gmail.com>
2026-06-05 11:18:05 -0600

    Added Reload

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,8 +1,9 @@
 extends Area2D  
 class_name Player
 @export var bullet_scene: PackedScene
-@export var fire_rate := 0.5
+@export var fire_rate := 0.3
 @onready var firetimer = $FireRate
+@onready var reloadTimer = $ReloadTimer
 
 var max_speed := 800
 var velocity = Vector2(0,0)
@@ -15,10 +16,20 @@ var a = null
 var queue_num = 0
 var timed = 0
 
+#gun variables
+var is_firing := false
+var max_ammo := 20
+var current_ammo := 20
+var reload_time := 1.5
+var current_reload := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_health(max_health)
 	add_to_group("Player")
+	firetimer.wait_time = fire_rate
+	reloadTimer.wait_time = reload_time
+	
 	print("Player layers: ", collision_layer)
 	print("Player mask: ", collision_mask)
 	print("Player instance: ", get_instance_id())
@@ -54,9 +65,27 @@ func shoot_weapon():
 	bullet.rotation = (mouse_pos - global_position).angle()
 	get_tree().current_scene.add_child(bullet)
 	
+func reload():
+	if current_reload:
+		return
+	
+	is_firing = false
+	print("Reloading...")
+	current_reload = true
+	reloadTimer.start()
+	
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		shoot_weapon()
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			print("Weapon firing")
+			is_firing = true
+			if current_ammo > 0:
+				shoot_weapon()
+				firetimer.start()
+		else:
+			print("Weapon released")
+			is_firing = false
+			firetimer.stop()
 		
 func set_health(new_health: int) -> void:
 	print("Original health: ", health)
@@ -110,6 +139,31 @@ func _on_timer_timeout() -> void: #continuous damage timer
 		get_node("Timer").stop()
 
 
-func _on_fire_rate_timeout() -> void: #firing speed timer
-	
+func _on_fire_rate_timeout() -> void:
+	if current_ammo <= 0:
+		reload()
+		return
+		
+	if is_firing:
+		shoot_weapon()
+		current_ammo -= 1
+		print(current_ammo)
+		
+		if current_ammo > 0:
+			firetimer.start()
+		else:
+			reload()
+			return
+		pass # Replace with function body.
+
+
+func _on_reload_timer_timeout() -> void: #reload timer
+	if current_reload:
+		current_ammo = max_ammo
+		print("Reloaded")
+		current_reload = false
+		
+		if is_firing:
+			firetimer.start()
+		
 	pass # Replace with function body.
