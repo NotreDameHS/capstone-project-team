a66222f1ee595763d28f258fcca7daef2dc1059c
ojrkarganilla <ojrkarganilla@gmail.com>
2026-06-08 11:11:34 -0600

    yea

--- entities/player/bullet.gd
+++ entities/player/bullet.gd
@@ -17,9 +17,44 @@ func _physics_process(delta: float) -> void:
 		_explode() 
 		
 func _explode() -> void:
+	poof(global_position)
 	queue_free()
 
+func poof(projectile_position: Vector2):
+	var particles = CPUParticles2D.new()
+	get_tree().current_scene.add_child(particles)
+	particles.global_position = projectile_position
+	# Create a particle cloud (a "poof" of particles from the center)
+	particles.z_index = 100 
+	particles.z_as_relative = false 
+	particles.amount = 20
+	particles.lifetime = 0.5
+	particles.explosiveness = 1.0
+	particles.one_shot = true
+	particles.scale_amount_min = 10.0 
+	particles.scale_amount_max = 20.0
+	particles.spread = 180.0
+	particles.gravity = Vector2(0, 0)
+	particles.initial_velocity_min = 80.0
+	particles.initial_velocity_max = 150.0
+	particles.damping_min = 50.0 
 
+	# Design the shape of the cloud (the "poof")
+	var curve = Curve.new()
+	curve.add_point(Vector2(0, 1.0)) 
+	curve.add_point(Vector2(1, 0.0))
+	particles.scale_amount_curve = curve
+
+	# Design the colours of the cloud
+	var gradient = Gradient.new()
+	gradient.add_point(0.0, Color(1, 1, 1, 1)) 
+	gradient.add_point(1.0, Color(1, 1, 1, 0)) 
+	particles.color_ramp = gradient
+	
+	particles.emitting = true
+	var timer = get_tree().create_timer(particles.lifetime + 0.5)
+	timer.timeout.connect(particles.queue_free)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
